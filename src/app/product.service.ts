eadc0733748441cfe2e4cd38919f2a760844b017
Zahid Zaman <zzaman@gmail.com>
2019-12-27 14:45:04 -0600

    Small fix.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -17,8 +17,6 @@ export class ProductService {
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map(response => 
-      <Product[]> response.json());
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
-
 }
